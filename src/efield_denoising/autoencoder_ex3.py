1453e4a1c34245edbeafec4e3241a4c2d0133997
Claire Guépin <claire.guepin21@gmail.com>
2024-01-30 18:04:11 +0100

    correct label and test input distributions

--- src/efield_denoising/autoencoder_ex3.py
+++ src/efield_denoising/autoencoder_ex3.py
@@ -26,21 +26,32 @@ plt.rc('font', family='serif')
 # CREATE DATASETS
 
 path = '/Users/claireguepin/Projects/GRAND/'\
-    + 'ML_efield_recons/Recons_Peak_Voltage_Train5000_Epoch100_conv3d_NAdam/'
+    + 'ML_efield_recons/Recons_Peak_Voltage_Train5000_Epoch100_conv3d_RAdam/'
 
 voltage = torch.load(path+'voltage_traces_p.pt')
 info = pandas.read_csv(path+'info_p.csv')
+
+# PARAMETERS
+indmax = 1000.
+tbin = 0.5  # ns
+
+nsim = 1054
+nant = 5
+num_train = 800
+num_valid = 100
 
 # FEATURES
 
 volmax = torch.max(torch.abs(voltage))
 voltage /= volmax
+voltage = voltage[:, ::4, :]
+ntime = np.shape(voltage)[1]
 print(np.shape(voltage))
 
 pos = np.array([[info["DU x"], info["DU y"], info["DU z"]]])
 posmax = np.max(pos)
 pos /= posmax
-pos = torch.tensor(pos.repeat(1000, 0))
+pos = torch.tensor(pos.repeat(ntime, 0))
 pos = torch.permute(pos, (2, 0, 1))
 print(np.shape(pos))
 
@@ -48,28 +59,27 @@ print(np.shape(pos))
 features = torch.cat((voltage, pos), dim=2)
 print(np.shape(features))
 # Then separate into batches of 5 traces per event (5270/5 = 1054)
-features = torch.reshape(features, (1054, 5, 1000, 6))
+features = torch.reshape(features, (nsim, nant, ntime, 6))
 print(np.shape(features))
 
 vol = np.array([[np.sqrt(info["Sum V^2"]/max(info["Sum V^2"]))]])
-voltot = torch.tensor(vol.repeat(1000, 1)).mT
-voltot = torch.reshape(voltot, (1054, 5, 1000, 1))
+voltot = torch.tensor(vol.repeat(ntime, 1)).mT
+voltot = torch.reshape(voltot, (nsim, nant, ntime, 1))
 print(np.shape(voltot))
 
 # LABELS
 
 ind = np.array([[info["Ind peak time"]]]).astype(float)
-indmax = 1000.
 ind /= indmax
-ind = torch.tensor(ind.repeat(1000, 1)).mT
-ind = torch.reshape(ind, (1054, 5, 1000, 1))
+ind = torch.tensor(ind.repeat(ntime, 1)).mT
+ind = torch.reshape(ind, (nsim, nant, ntime, 1))
 print(np.shape(ind))
 
-val = np.array([[info["Val peak amplitude"]]])
-val = torch.tensor(val.repeat(1000, 1)).mT
+val = np.log10(np.array([[info["Val peak amplitude"]]]))
+val = torch.tensor(val.repeat(ntime, 1)).mT
 valmax = torch.max(torch.abs(val))
 val /= valmax
-val = torch.reshape(val, (1054, 5, 1000, 1))
+val = torch.reshape(val, (nsim, nant, ntime, 1))
 print(np.shape(val))
 
 # Concatenate everything
@@ -77,8 +87,8 @@ data = torch.cat((features, voltot, ind, val), dim=3)
 data = torch.permute(data, (0, 1, 3, 2))
 print(np.shape(data))
 
-num_train = 800
-train, valid, test = torch.split(data, (num_train, 100, 1054-100-num_train), 0)
+train, valid, test = torch.split(data, (num_train, num_valid,
+                                        nsim-num_valid-num_train), 0)
 
 batch_size = 1
 
@@ -93,6 +103,37 @@ valid_loader = torch.utils.data.DataLoader(valid,
 test_loader = torch.utils.data.DataLoader(test,
                                           batch_size=1,
                                           shuffle=True)
+
+# Visualize distributions of labels
+# Make sure they are not going to biais results (how?)
+
+# The index distribution does not seem flat, which is intriguing
+
+# ind = np.array([[info["Ind peak time"]]]).astype(float)[0, 0, :]
+# # ind /= indmax
+# plt.figure()
+# ax = plt.gca()
+# plt.hist(ind, bins=10, alpha=0.5)
+# # plt.hist(ind_reco, bins=10, alpha=0.5,
+# #          label='iron')
+# ax.xaxis.set_ticks_position('both')
+# ax.yaxis.set_ticks_position('both')
+# ax.tick_params(labelsize=14)
+
+# In log10 val peak alplitude looks like a Gaussian
+
+# # val = np.array([[info["Val peak amplitude"]]])[0, 0, :]
+# val = np.log10(np.array([[info["Val peak amplitude"]]]))[0, 0, :]
+# valmax = max(np.abs(val))
+# val /= valmax
+# plt.figure()
+# ax = plt.gca()
+# plt.hist(val, bins=10, alpha=0.5)
+# # plt.hist(ind_reco, bins=10, alpha=0.5,
+# #          label='iron')
+# ax.xaxis.set_ticks_position('both')
+# ax.yaxis.set_ticks_position('both')
+# ax.tick_params(labelsize=14)
 
 # =============================================================================
 # ENCODER AND DECODER
@@ -116,11 +157,12 @@ class Net(nn.Module):
         self.flatten = nn.Flatten(start_dim=0)
         # Linear section
         self.encoder_lin = nn.Sequential(
-            nn.Linear(16 * 250+1, fc2_input_dim),
+            # nn.Linear(16 * 250+1, fc2_input_dim),
+            nn.Linear(16 * 62+1, fc2_input_dim),
             nn.ReLU(True),
         )
         self.encoder_lin2 = nn.Sequential(
-            nn.Linear(fc2_input_dim, 2)
+            nn.Linear(fc2_input_dim, 10),
         )
 
     def forward(self, x):
@@ -137,6 +179,7 @@ class Net(nn.Module):
         x = torch.cat((x, xloc))
         x = self.encoder_lin(x)
         x = self.encoder_lin2(x)
+        x = torch.reshape(x, (5, 2))
         # print(np.shape(x))
         return x
 
@@ -152,7 +195,9 @@ def train_epoch_den(encoder, device, dataloader, loss_fn, optimizer):
     for data in dataloader:
         iterloc += 1
         features = data[:, :, :7, :]
-        labels = data[0, 0, 7:10, 0]
+        # print(np.shape(features))
+        labels = data[0, :, 7:10, 0]
+        # print(np.shape(labels))
 
         # Move tensor to the proper device
         labels = labels.to(device)
@@ -166,6 +211,9 @@ def train_epoch_den(encoder, device, dataloader, loss_fn, optimizer):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
+        # scheduler.step()
+
         # Print batch loss
         # print('\t partial train loss (single batch): %f' % (loss.data))
         train_loss.append(loss.detach().cpu().numpy())
@@ -185,7 +233,7 @@ def test_epoch_den(encoder, device, dataloader, loss_fn):
         conc_label = []
         for data in dataloader:
             features = data[:, :, :7, :]
-            labels = data[0, 0, 7:10, 0]
+            labels = data[0, :, 7:10, 0]
 
             # Move tensor to the proper device
             features = features.to(device)
@@ -215,15 +263,22 @@ loss_fn = torch.nn.MSELoss()
 torch.manual_seed(0)
 
 # Initialize the network
-net = Net(fc2_input_dim=8).double()
+net = Net(fc2_input_dim=16).double()
 params_to_optimize = net.parameters()
 
-optimtype = torch.optim.Adam(params_to_optimize)
+# optimtype = torch.optim.Adam(params_to_optimize)
 # optimtype = torch.optim.NAdam(params_to_optimize)
-# optimtype = torch.optim.RAdam(params_to_optimize)
+optimtype = torch.optim.RAdam(params_to_optimize)
+# optimtype = torch.optim.RAdam(params_to_optimize, weight_decay=1e-3)
 
 # we can also implement momentum decay, results are quite sensitive to it
 # optimtype = torch.optim.NAdam(params_to_optimize, momentum_decay=0.003)
+
+# optimtype = torch.optim.SGD(params_to_optimize)
+
+# scheduler = torch.optim.lr_scheduler.CyclicLR(optimtype,
+#                                               base_lr=0.01,
+#                                               max_lr=0.1)
 
 # Check if the GPU is available
 if torch.cuda.is_available():
@@ -239,7 +294,7 @@ net.to(device)
 # =============================================================================
 # TRAINING
 
-num_epochs = 100
+num_epochs = 10
 history_da = {'train_loss': [], 'val_loss': []}
 
 print('\n TRAINING and VALIDATION')
@@ -268,23 +323,16 @@ for epoch in range(num_epochs):
 # =============================================================================
 # VISUALIZE LOSS
 
-# it_train = 0
-# MSE = 0
-# for train_sig in train_loader:
-#     it_train += 1
-#     efield = train_sig[:, :, :, 0]
-#     MSE += np.sum(np.array(efield[0]**2))/len(efield[0][0])/len(efield[0])
-# print("MSE with recons of null function = %.2e" % (MSE/it_train))
-
 indobj = 10  # 5ns
-valobj = 0.2  # 20%
+valobj = 0.10  # 5-10%
 
 it_train = 0
 MSE = 0
 for train_sig in train_loader:
-    it_train += 1
-    ampl = train_sig[0, 0, 8, 0].item()
-    MSE += (indobj/indmax)**2 + (valobj*ampl)**2
+    it_train += 5
+    # ampl = 10**np.array(train_sig[0, :, 8, 0])
+    # MSE += 5.*(indobj/indmax)**2 + np.sum((valobj*ampl)**2)
+    MSE += 5.*(indobj/indmax)**2 + 5.*np.log10(1.-valobj)**2
 MSE /= it_train
 
 fig = plt.figure()
@@ -333,145 +381,61 @@ plt.show()
 # =============================================================================
 # VISUALIZE SOME TRAIN and TEST results
 
+
+def vis_hist(data_loader, namefig):
+    """Visualize histograms."""
+    ind_real = np.empty([0])
+    ind_reco = np.empty([0])
+    val_real = np.empty([0])
+    val_reco = np.empty([0])
+    for data in data_loader:
+        ind_real = np.append(ind_real, data[0, :, 7, 0])
+        val_real = np.append(val_real, data[0, :, 8, 0])
+        features = data[:, :, 0:7, :]
+        features = features.to(device)
+        net.eval()
+        with torch.no_grad():
+            rec_sig = (net(features))
+        ind_reco = np.append(ind_reco, rec_sig[:, 0])
+        val_reco = np.append(val_reco, rec_sig[:, 1])
+
+    plt.figure()
+    ax = plt.gca()
+    plt.hist((ind_real-ind_reco)*indmax*tbin, bins=10, alpha=0.5)
+    # plt.hist(ind_reco, bins=10, alpha=0.5,
+    #          label='iron')
+    ax.xaxis.set_ticks_position('both')
+    ax.yaxis.set_ticks_position('both')
+    ax.tick_params(labelsize=14)
+    mean = np.mean((ind_real-ind_reco)*indmax*tbin)
+    std = np.std((ind_real-ind_reco)*indmax*tbin)
+    ax.set_title(r"Histogram Time (i_{\rm real}-i_{\rm reco})"
+                 + "\n"+r"Mean: %.1f, Std: %.1f" % (mean, std), fontsize=14)
+    # ax.legend(prop={'size': 12}, frameon=False)
+    plt.savefig(path+"Hist_ind_"+namefig+".pdf")
+
+    plt.figure()
+    ax = plt.gca()
+    plt.hist((10**val_real-10**val_reco)/10**val_real, bins=10, alpha=0.5)
+    # plt.hist(ind_reco, bins=10, alpha=0.5,
+    #          label='iron')
+    ax.xaxis.set_ticks_position('both')
+    ax.yaxis.set_ticks_position('both')
+    ax.tick_params(labelsize=14)
+    mean = np.mean((val_real-val_reco)/val_real)
+    std = np.std((val_real-val_reco)/val_real)
+    ax.set_title(r"Histogram Amplitude"
+                 + r"(A_{\rm real}-A_{\rm reco})/A_{\rm real}"
+                 + "\n"+r"Mean: %.1e, Std: %.1e" % (mean, std), fontsize=14)
+    # ax.legend(prop={'size': 12}, frameon=False)
+    plt.savefig(path+"Hist_val_"+namefig+".pdf")
+
+
 # TRAIN
-
-ind_real = np.empty([0])
-ind_reco = np.empty([0])
-val_real = np.empty([0])
-val_reco = np.empty([0])
-for data in train_loader:
-    ind_real = np.append(ind_real, data[0, 0, 7, 0])
-    val_real = np.append(val_real, data[0, 0, 8, 0])
-    features = data[:, :, 0:7, :]
-    features = features.to(device)
-    net.eval()
-    with torch.no_grad():
-        rec_sig = (net(features))
-    ind_reco = np.append(ind_reco, rec_sig[0])
-    val_reco = np.append(val_reco, rec_sig[1])
-
-plt.figure()
-ax = plt.gca()
-plt.hist((ind_real-ind_reco)*1000, bins=10, alpha=0.5)
-# plt.hist(ind_reco, bins=10, alpha=0.5,
-#          label='iron')
-ax.xaxis.set_ticks_position('both')
-ax.yaxis.set_ticks_position('both')
-ax.tick_params(labelsize=14)
-mean = np.mean((ind_real-ind_reco)*1000)
-std = np.std((ind_real-ind_reco)*1000)
-ax.set_title(r"Histogram Time index (i_{\rm real}-i_{\rm reco})"
-             + "\n"+r"Mean: %.1f, Std: %.1f" % (mean, std), fontsize=14)
-# ax.legend(prop={'size': 12}, frameon=False)
-plt.savefig(path+"Hist_ind_train.pdf")
-
-plt.figure()
-ax = plt.gca()
-plt.hist((val_real-val_reco)/val_real, bins=10, alpha=0.5)
-# plt.hist(ind_reco, bins=10, alpha=0.5,
-#          label='iron')
-ax.xaxis.set_ticks_position('both')
-ax.yaxis.set_ticks_position('both')
-ax.tick_params(labelsize=14)
-mean = np.mean((val_real-val_reco)/val_real)
-std = np.std((val_real-val_reco)/val_real)
-ax.set_title(r"Histogram Amplitude (A_{\rm real}-A_{\rm reco})/A_{\rm real}"
-             + "\n"+r"Mean: %.1e, Std: %.1e" % (mean, std), fontsize=14)
-# ax.legend(prop={'size': 12}, frameon=False)
-plt.savefig(path+"Hist_val_train.pdf")
+vis_hist(train_loader, 'train')
 
 # VALIDATION
-
-ind_real = np.empty([0])
-ind_reco = np.empty([0])
-val_real = np.empty([0])
-val_reco = np.empty([0])
-for data in valid_loader:
-    ind_real = np.append(ind_real, data[0, 0, 7, 0])
-    val_real = np.append(val_real, data[0, 0, 8, 0])
-    features = data[:, :, 0:7, :]
-    features = features.to(device)
-    net.eval()
-    with torch.no_grad():
-        rec_sig = (net(features))
-    ind_reco = np.append(ind_reco, rec_sig[0])
-    val_reco = np.append(val_reco, rec_sig[1])
-
-plt.figure()
-ax = plt.gca()
-plt.hist((ind_real-ind_reco)*1000, bins=10, alpha=0.5)
-# plt.hist(ind_reco, bins=10, alpha=0.5,
-#          label='iron')
-ax.xaxis.set_ticks_position('both')
-ax.yaxis.set_ticks_position('both')
-ax.tick_params(labelsize=14)
-mean = np.mean((ind_real-ind_reco)*1000)
-std = np.std((ind_real-ind_reco)*1000)
-ax.set_title(r"Histogram Time index (i_{\rm real}-i_{\rm reco})"
-             + "\n"+r"Mean: %.1f, Std: %.1f" % (mean, std), fontsize=14)
-# ax.legend(prop={'size': 12}, frameon=False)
-plt.savefig(path+"Hist_ind_valid.pdf")
-
-
-plt.figure()
-ax = plt.gca()
-plt.hist((val_real-val_reco)/val_real, bins=10, alpha=0.5)
-# plt.hist(ind_reco, bins=10, alpha=0.5,
-#          label='iron')
-ax.xaxis.set_ticks_position('both')
-ax.yaxis.set_ticks_position('both')
-ax.tick_params(labelsize=14)
-mean = np.mean((val_real-val_reco)/val_real)
-std = np.std((val_real-val_reco)/val_real)
-ax.set_title(r"Histogram Amplitude (A_{\rm real}-A_{\rm reco})/A_{\rm real}"
-             + "\n"+r"Mean: %.1e, Std: %.1e" % (mean, std), fontsize=14)
-# ax.legend(prop={'size': 12}, frameon=False)
-plt.savefig(path+"Hist_val_valid.pdf")
+vis_hist(valid_loader, 'valid')
 
 # TEST
-
-ind_real = np.empty([0])
-ind_reco = np.empty([0])
-val_real = np.empty([0])
-val_reco = np.empty([0])
-for data in test_loader:
-    ind_real = np.append(ind_real, data[0, 0, 7, 0])
-    val_real = np.append(val_real, data[0, 0, 8, 0])
-    features = data[:, :, 0:7, :]
-    features = features.to(device)
-    net.eval()
-    with torch.no_grad():
-        rec_sig = (net(features))
-    ind_reco = np.append(ind_reco, rec_sig[0])
-    val_reco = np.append(val_reco, rec_sig[1])
-
-plt.figure()
-ax = plt.gca()
-plt.hist((ind_real-ind_reco)*1000, bins=10, alpha=0.5)
-# plt.hist(ind_reco, bins=10, alpha=0.5,
-#          label='iron')
-ax.xaxis.set_ticks_position('both')
-ax.yaxis.set_ticks_position('both')
-ax.tick_params(labelsize=14)
-mean = np.mean((ind_real-ind_reco)*1000)
-std = np.std((ind_real-ind_reco)*1000)
-ax.set_title(r"Histogram Time index (i_{\rm real}-i_{\rm reco})"
-             + "\n"+r"Mean: %.1f, Std: %.1f" % (mean, std), fontsize=14)
-# ax.legend(prop={'size': 12}, frameon=False)
-plt.savefig(path+"Hist_ind_test.pdf")
-
-
-plt.figure()
-ax = plt.gca()
-plt.hist((val_real-val_reco)/val_real, bins=10, alpha=0.5)
-# plt.hist(ind_reco, bins=10, alpha=0.5,
-#          label='iron')
-ax.xaxis.set_ticks_position('both')
-ax.yaxis.set_ticks_position('both')
-ax.tick_params(labelsize=14)
-mean = np.mean((val_real-val_reco)/val_real)
-std = np.std((val_real-val_reco)/val_real)
-ax.set_title(r"Histogram Amplitude (A_{\rm real}-A_{\rm reco})/A_{\rm real}"
-             + "\n"+r"Mean: %.1e, Std: %.1e" % (mean, std), fontsize=14)
-# ax.legend(prop={'size': 12}, frameon=False)
-plt.savefig(path+"Hist_val_test.pdf")
+vis_hist(test_loader, 'test')
